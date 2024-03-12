8c865e56dbb014421e3c62298a2d9caf02a4da2c
Amha Desta <amha7819@gmail.com>
2024-03-11 21:38:05 -0500

    env var test

--- topHeadline.js
+++ topHeadline.js
@@ -3,23 +3,29 @@ dotenv.config();
 
 const apiKey = process.env.API_KEY;
 const url = `https://newsapi.org/v2/top-headlines/sources?apiKey=${apiKey}`;
-const topHLs = process.env.ADDITIONAL_SOURCES.split(", ");
+const srcs = process.env.ADDITIONAL_SOURCES;
+console.log(`type of and val: ${typeof srcs} - ${srcs}`);
+const topHLs = srcs.split(":");
 
-async function getTopHeadlineSrcs() {
-  try {
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error("Network response for fetch newsapi top headlines was not ok");
-    }
 
-    const data = await res.json();
-    for (const src of data.sources) {
-      if (src.name) topHLs.push(src.name);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-  return topHLs;
-}
+for (let i of topHLs)
+  console.log(`top headlines test: ${i}`)
 
-export { getTopHeadlineSrcs };
+// async function getTopHeadlineSrcs() {
+//   try {
+//     const res = await fetch(url);
+//     if (!res.ok) {
+//       throw new Error("Network response for fetch newsapi top headlines was not ok");
+//     }
+
+//     const data = await res.json();
+//     for (const src of data.sources) {
+//       if (src.name) topHLs.push(src.name);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   return topHLs;
+// }
+
+// export { getTopHeadlineSrcs };
